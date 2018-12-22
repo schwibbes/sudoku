c6e8f5595fcfb6f8a6a0ae6d25fa047ffacef2f2
schwibbes2 <schwibbes2@gmail.com>
2018-12-22 23:40:18 +0100

    reagent hello-world

--- src/hello_world/core.cljs
+++ src/hello_world/core.cljs
@@ -1,10 +1,28 @@
-(ns hello-world.core)
+(ns ^:figwheel-hooks hello-world.core
+  (:require
+   [goog.dom :as gdom]
+   [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
 
-(defn hello [] "hello There")
+(defonce app-state (atom {:text "Hello world!"}))
 
-;; uncomment this to alter the provided "app" DOM element
-;; (set! (.-innerHTML (js/document.getElementById "app")) (hello))
+(defn get-app-element []
+  (gdom/getElement "app"))
 
-(println (hello))
+(defn hello-world [] 
+  [:div
+   [:h1 (:text @app-state)]
+   [:h3 "watch it  change!"]])
+
+(defn mount [el]
+  (reagent/render-component [hello-world] el))
+
+(defn mount-app-element []
+  (when-let [el (get-app-element)]
+    (mount el)))
+
+(mount-app-element)
+
+(defn ^:after-load on-reload []
+  (mount-app-element))
