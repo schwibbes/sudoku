ee45d0ca3d793a657372caba265bbc94c3a92928
schwibbes2 <schwibbes2@gmail.com>
2018-12-23 20:46:30 +0100

    fix production build

--- prod.sh
+++ prod.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
- clojure -m figwheel.main --optimizations advanced --build-once dev
+rm -rf build/
+cp -r resources/public/* docs/
+clojure -m figwheel.main --optimizations advanced -o docs/cljs-out/dev-main.js --output-dir "build/" --build-once dev
